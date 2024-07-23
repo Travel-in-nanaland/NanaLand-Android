7a9bc3799dbf524f1b13d67e2ee4d07cdc012a51
WorldOneTop <dlwpdlf147@naver.com>
2024-07-24 00:10:35 +0900

    [revert] 기존 마이 페이지 수정 이전으로 되돌리기 (삭제예정)

--- app/src/main/java/com/jeju/nanaland/ui/main/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/jeju/nanaland/ui/main/mypage/MyPageViewModel.kt
@@ -5,8 +5,6 @@ import androidx.lifecycle.viewModelScope
 import com.jeju.nanaland.domain.entity.member.UserProfile
 import com.jeju.nanaland.domain.usecase.member.GetUserProfileUseCase
 import com.jeju.nanaland.globalvalue.userdata.UserData
-import com.jeju.nanaland.ui.component.mypage.TempNoticeData
-import com.jeju.nanaland.ui.component.mypage.TempReviewData
 import com.jeju.nanaland.util.log.LogUtil
 import com.jeju.nanaland.util.network.onError
 import com.jeju.nanaland.util.network.onException
@@ -19,11 +17,7 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Date
 import javax.inject.Inject
-import kotlin.random.Random
 
 @HiltViewModel
 class MyPageViewModel @Inject constructor(
@@ -32,15 +26,6 @@ class MyPageViewModel @Inject constructor(
 
     private val _userProfile = MutableStateFlow<UiState<UserProfile>>(UiState.Loading)
     val userProfile = _userProfile.asStateFlow()
-
-    private val _reviews = MutableStateFlow<List<TempReviewData>>(emptyList())
-    val reviews = _reviews.asStateFlow()
-    private val _notices = MutableStateFlow<List<TempNoticeData>>(emptyList())
-    val notices = _notices.asStateFlow()
-
-    init {
-        fakeApiCall()
-    }
 
     fun getUserProfile() {
         getUserProfileUseCase()
@@ -65,27 +50,5 @@ class MyPageViewModel @Inject constructor(
             }
             .catch { LogUtil.e("flow Error", "getUserProfileUseCase") }
             .launchIn(viewModelScope)
-    }
-
-    private fun fakeApiCall() {
-        _reviews.update {
-            List(10) {
-                TempReviewData(
-                    "우진이네 해장국",
-                    if(Random.nextBoolean()) "https://picsum.photos/100" else null,
-                    Date(),
-                    it
-                )
-            }
-        }
-        _notices.update {
-            List(8) {
-                TempNoticeData(
-                    Random.nextInt(1,3),
-                    "${it + 1}월 1주차 공지 or 개편",
-                    SimpleDateFormat("yyyy.MM.dd").parse("2024.0${it + 1}.01")!!
-                )
-            }
-        }
     }
 }
