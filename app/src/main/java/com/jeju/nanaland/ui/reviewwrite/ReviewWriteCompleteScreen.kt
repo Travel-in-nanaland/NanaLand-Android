a6f91095cbce71de24378ebcc0d54670a7bf2d49
WorldOneTop <dlwpdlf147@naver.com>
2024-08-10 03:05:56 +0900

    [fix] fix review write screen

--- app/src/main/java/com/jeju/nanaland/ui/reviewwrite/ReviewWriteCompleteScreen.kt
+++ app/src/main/java/com/jeju/nanaland/ui/reviewwrite/ReviewWriteCompleteScreen.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -66,7 +69,8 @@ private fun ReviewWriteCompleteUI(
 
             LottieAnimation(
                 modifier = Modifier
-                    .fillMaxWidth(),
+                    .fillMaxWidth()
+                    .padding(horizontal = 55.dp),
                 composition = composition,
                 progress = { progress },
             )
