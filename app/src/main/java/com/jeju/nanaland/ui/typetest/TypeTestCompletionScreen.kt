0de952a049172212a7937021cfaaf9a3a86ad2b6
WorldOneTop <dlwpdlf147@naver.com>
2024-09-06 23:58:03 +0900

    [fix] 테스트 결과 로딩 Gif 적용

--- app/src/main/java/com/jeju/nanaland/ui/typetest/TypeTestCompletionScreen.kt
+++ app/src/main/java/com/jeju/nanaland/ui/typetest/TypeTestCompletionScreen.kt
@@ -1,21 +1,21 @@
 package com.jeju.nanaland.ui.typetest
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.jeju.nanaland.R
 import com.jeju.nanaland.ui.component.common.CustomSurface
 import com.jeju.nanaland.ui.component.signup.typetestcompletion.TypeTestCompletionScreenBottomButton
 import com.jeju.nanaland.ui.component.signup.typetestcompletion.TypeTestCompletionScreenText
-import com.jeju.nanaland.ui.theme.getColor
 
 @Composable
 fun TypeTestCompletionScreen(
@@ -32,61 +32,20 @@ private fun TypeTestCompletionScreen(
     moveToTypeTestLoadingScreen: () -> Unit,
     isContent: Boolean
 ) {
+    val gif by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loading_for_test_type))
+
     CustomSurface {
         Spacer(Modifier.height(110.dp))
 
-        Column(
+        TypeTestCompletionScreenText()
+
+        LottieAnimation(
             modifier = Modifier
-                .padding(start = 16.dp, end = 16.dp)
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            TypeTestCompletionScreenText()
-
-            Spacer(Modifier.height(50.dp))
-
-            Spacer(
-                modifier = Modifier
-                    .size(24.dp)
-                    .background(
-                        color = getColor().main,
-                        shape = RoundedCornerShape(50)
-                    )
-            )
-
-            Spacer(Modifier.height(40.dp))
-
-            Spacer(
-                modifier = Modifier
-                    .size(16.dp)
-                    .background(
-                        color = getColor().main50,
-                        shape = RoundedCornerShape(50)
-                    )
-            )
-
-            Spacer(Modifier.height(40.dp))
-
-            Spacer(
-                modifier = Modifier
-                    .size(10.dp)
-                    .background(
-                        color = getColor().main10,
-                        shape = RoundedCornerShape(50)
-                    )
-            )
-
-            Spacer(Modifier.height(40.dp))
-
-            Spacer(
-                modifier = Modifier
-                    .size(10.dp)
-                    .background(
-                        color = getColor().main10,
-                        shape = RoundedCornerShape(50)
-                    )
-            )
-        }
+                .heightIn(max = 175.dp)
+                .align(Alignment.CenterHorizontally),
+            composition = gif,
+            iterations = LottieConstants.IterateForever
+        )
 
         Spacer(Modifier.weight(1f))
 
