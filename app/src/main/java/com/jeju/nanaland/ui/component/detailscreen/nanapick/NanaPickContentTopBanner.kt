1f8c5acfcf7cf983ada0f299aec8f4bc8432996f
WorldOneTop <dlwpdlf147@naver.com>
2024-09-07 08:32:44 +0900

    [MVP2-241] 나나스픽 변경

--- app/src/main/java/com/jeju/nanaland/ui/component/detailscreen/nanapick/NanaPickContentTopBanner.kt
+++ app/src/main/java/com/jeju/nanaland/ui/component/detailscreen/nanapick/NanaPickContentTopBanner.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -18,18 +22,19 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.jeju.nanaland.R
 import com.jeju.nanaland.domain.entity.nanapick.NanaPickContentData
-import com.jeju.nanaland.ui.component.detailscreen.nanapick.parts.topbanner.NanaPickContentTopBannerSubTitle
-import com.jeju.nanaland.ui.component.detailscreen.nanapick.parts.topbanner.NanaPickContentTopBannerTitle
-import com.jeju.nanaland.util.ui.UiState
 import com.jeju.nanaland.ui.component.detailscreen.nanapick.parts.topbanner.NanaPickContentTopBannerFavoriteButton
 import com.jeju.nanaland.ui.component.detailscreen.nanapick.parts.topbanner.NanaPickContentTopBannerShareButton
-import com.jeju.nanaland.ui.component.detailscreen.nanapick.parts.topbanner.NanaPickContentTopBannerVersion
+import com.jeju.nanaland.ui.component.detailscreen.nanapick.parts.topbanner.NanaPickContentTopBannerSubTitle
+import com.jeju.nanaland.ui.component.detailscreen.nanapick.parts.topbanner.NanaPickContentTopBannerTitle
 import com.jeju.nanaland.ui.theme.getColor
 import com.jeju.nanaland.util.language.getLanguage
 import com.jeju.nanaland.util.ui.ScreenPreview
+import com.jeju.nanaland.util.ui.UiState
+import com.jeju.nanaland.util.ui.clickableNoEffect
 import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
@@ -37,6 +42,7 @@ fun NanaPickContentTopBanner(
     height: Int,
     contentId: Int?,
     nanaPickContent: UiState.Success<NanaPickContentData>,
+    onBackButtonClicked: () -> Unit,
     toggleFavorite: () -> Unit,
     moveToSignInScreen: () -> Unit,
 ) {
@@ -65,45 +71,51 @@ fun NanaPickContentTopBanner(
                     )
                 }
         )
-
-        NanaPickContentTopBannerVersion(
-            modifier = Modifier.align(Alignment.TopStart),
-            text = ""
-        )
-
         Column(
-            modifier = Modifier
-                .align(Alignment.BottomStart)
-                .padding(start = 16.dp, bottom = 20.dp)
+            Modifier.padding(horizontal = 16.dp)
         ) {
-            NanaPickContentTopBannerSubTitle(text = nanaPickContent.data.subHeading)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 12.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .size(32.dp)
+                        .clickableNoEffect { onBackButtonClicked() },
+                    painter = painterResource(id = R.drawable.ic_arrow_left),
+                    contentDescription = null,
+                    tint = getColor().white
+                )
 
-            NanaPickContentTopBannerTitle(text = nanaPickContent.data.heading)
-        }
+                Spacer(modifier = Modifier.weight(1f))
 
-        NanaPickContentTopBannerShareButton(
-            modifier = Modifier
-                .align(Alignment.TopEnd)
-                .padding(top = 8.dp, end = 16.dp),
-            onClick = {
-                val sendIntent: Intent = Intent().apply {
-                    action = Intent.ACTION_SEND
-                    putExtra(Intent.EXTRA_TEXT, "http://13.125.110.80:8080/share/${getLanguage()}?category=nanapick&id=${contentId}")
-                    type = "text/plain"
-                }
-                val shareIntent = Intent.createChooser(sendIntent, null)
-                context.startActivity(shareIntent)
+                NanaPickContentTopBannerShareButton(
+                    onClick = {
+                        val sendIntent: Intent = Intent().apply {
+                            action = Intent.ACTION_SEND
+                            putExtra(Intent.EXTRA_TEXT, "http://13.125.110.80:8080/share/${getLanguage()}?category=nanapick&id=${contentId}")
+                            type = "text/plain"
+                        }
+                        val shareIntent = Intent.createChooser(sendIntent, null)
+                        context.startActivity(shareIntent)
+                    }
+                )
+
+                NanaPickContentTopBannerFavoriteButton(
+                    isFavorite = nanaPickContent.data.favorite,
+                    onClick = { toggleFavorite() },
+                    moveToSignInScreen = moveToSignInScreen,
+                )
             }
-        )
 
-        NanaPickContentTopBannerFavoriteButton(
-            modifier = Modifier
-                .align(Alignment.TopEnd)
-                .padding(top = 8.dp, end = 56.dp),
-            isFavorite = nanaPickContent.data.favorite,
-            onClick = { toggleFavorite() },
-            moveToSignInScreen = moveToSignInScreen,
-        )
+            Spacer(modifier = Modifier.weight(1f))
+
+            NanaPickContentTopBannerSubTitle(text = nanaPickContent.data.subHeading)
+            NanaPickContentTopBannerTitle(text = nanaPickContent.data.heading)
+            Spacer(modifier = Modifier.height(16.dp))
+        }
     }
 }
 
