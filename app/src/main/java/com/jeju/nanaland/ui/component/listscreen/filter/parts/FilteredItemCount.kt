3563970d6950ec16adececa12062b0535cfaf0fc
WorldOneTop <dlwpdlf147@naver.com>
2024-09-07 07:01:26 +0900

    [MVP2-236] 필터링 row - 00건 삭제

--- app/src/main/java/com/jeju/nanaland/ui/component/listscreen/filter/parts/FilteredItemCount.kt
+++ app/src/main/java/com/jeju/nanaland/ui/component/listscreen/filter/parts/FilteredItemCount.kt
@@ -1,23 +1,21 @@
 package com.jeju.nanaland.ui.component.listscreen.filter.parts
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
-import com.jeju.nanaland.R
 import com.jeju.nanaland.ui.theme.NanaLandTheme
-import com.jeju.nanaland.ui.theme.body02
-import com.jeju.nanaland.ui.theme.getColor
-import com.jeju.nanaland.util.resource.getString
 import com.jeju.nanaland.util.ui.ComponentPreview
 
 @Composable
 fun FilteredItemCount(
     count: Int
 ) {
-    Text(
-        text = "$count " + getString(R.string.common_건),
-        color = getColor().gray01,
-        style = body02
-    )
+    Box { }
+    // TODO 삭제
+//    Text(
+//        text = "$count " + getString(R.string.common_건),
+//        color = getColor().gray01,
+//        style = body02
+//    )
 }
 
 @ComponentPreview
