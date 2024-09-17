e1ba447be540d854940221a82fdc74b072c9c203
CodingHarang <95116599+CodingHarang@users.noreply.github.com>
2024-09-18 08:31:14 +0900

    [feat]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,7 @@ android {
         applicationId = "com.jeju.nanaland"
         minSdk = 26
         targetSdk = 34
-        versionCode = 33
-        versionCode = 31
+        versionCode = 34
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
