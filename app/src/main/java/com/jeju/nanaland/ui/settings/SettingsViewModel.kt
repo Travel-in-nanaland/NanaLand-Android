50191c0a23ec08cc6e3c39ff668bd725d9ae044b
WorldOneTop <dlwpdlf147@naver.com>
2024-10-17 23:46:46 +0900

    [fix] 언어 저장 로직 변경한 거 대응

--- app/src/main/java/com/jeju/nanaland/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/jeju/nanaland/ui/settings/SettingsViewModel.kt
@@ -3,21 +3,23 @@ package com.jeju.nanaland.ui.settings
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jeju.nanaland.domain.usecase.authdatastore.ClearAuthDataStoreUseCase
-import com.jeju.nanaland.domain.usecase.authdatastore.SaveAccessTokenUseCase
-import com.jeju.nanaland.domain.usecase.authdatastore.SaveRefreshTokenUseCase
 import com.jeju.nanaland.domain.usecase.member.SignOutUseCase
 import com.jeju.nanaland.domain.usecase.recentsearchdatastore.ClearRecentSearchDataStoreUseCase
 import com.jeju.nanaland.domain.usecase.settingsdatastore.ClearUserSettingsDataStoreUseCase
-import com.jeju.nanaland.globalvalue.constant.KEY_LANGUAGE
+import com.jeju.nanaland.domain.usecase.settingsdatastore.GetLanguageUseCase
+import com.jeju.nanaland.domain.usecase.settingsdatastore.SetLanguageUseCase
+import com.jeju.nanaland.globalvalue.type.LanguageType
 import com.jeju.nanaland.util.log.LogUtil
 import com.jeju.nanaland.util.network.onError
 import com.jeju.nanaland.util.network.onException
 import com.jeju.nanaland.util.network.onSuccess
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @HiltViewModel
@@ -26,8 +28,8 @@ class SettingsViewModel @Inject constructor(
     private val clearAuthDataStoreUseCase: ClearAuthDataStoreUseCase,
     private val clearUserSettingsDataStoreUseCase: ClearUserSettingsDataStoreUseCase,
     private val clearRecentSearchDataStoreUseCase: ClearRecentSearchDataStoreUseCase,
-    private val getValueUseCase: GetValueUseCase,
-    private val saveValueUseCase: SaveValueUseCase
+    private val getLanguageUseCase: GetLanguageUseCase,
+    private val setLanguageUseCase: SetLanguageUseCase
 ) : ViewModel() {
 
     fun signOut(
@@ -36,20 +38,14 @@ class SettingsViewModel @Inject constructor(
         signOutUseCase()
             .onEach { networkResult ->
                 networkResult.onSuccess { code, message, data ->
-                    var locale = "en"
-                    getValueUseCase(key = KEY_LANGUAGE)
-                        .onEach {
-                            locale = it ?: "en"
-                        }
-                        .catch { LogUtil.e("flow Error", "getValueUseCase") }
-                        .launchIn(viewModelScope)
+                    var locale:LanguageType
+                    runBlocking {
+                        locale = getLanguageUseCase().firstOrNull() ?: LanguageType.English
+                    }
                     clearAuthDataStoreUseCase()
                     clearUserSettingsDataStoreUseCase()
                     clearRecentSearchDataStoreUseCase()
-                    viewModelScope.launch { saveValueUseCase(
-                        key = KEY_LANGUAGE,
-                        value = locale
-                    ) }
+                    viewModelScope.launch { setLanguageUseCase(locale) }
                     moveToSignInScreen()
                 }.onError { code, message ->
 
