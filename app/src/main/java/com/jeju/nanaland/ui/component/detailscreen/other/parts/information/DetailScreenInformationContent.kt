6691687d1bb4575098fb8a329e8a0f897dddf9de
WorldOneTop <dlwpdlf147@naver.com>
2024-11-09 05:35:39 +0900

    [UT-UX-6][[UT-UX-17] 주소 밑줄

--- app/src/main/java/com/jeju/nanaland/ui/component/detailscreen/other/parts/information/DetailScreenInformationContent.kt
+++ app/src/main/java/com/jeju/nanaland/ui/component/detailscreen/other/parts/information/DetailScreenInformationContent.kt
@@ -1,7 +1,9 @@
 package com.jeju.nanaland.ui.component.detailscreen.other.parts.information
 
+import android.webkit.URLUtil
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextDecoration
 import com.jeju.nanaland.ui.theme.NanaLandTheme
 import com.jeju.nanaland.ui.theme.body02
 import com.jeju.nanaland.ui.theme.getColor
@@ -12,7 +14,8 @@ fun DetailScreenInformationContent(text: String?) {
     Text(
         text = text ?: "",
         color = getColor().black,
-        style = body02
+        style = body02,
+        textDecoration = if(URLUtil.isValidUrl(text)) TextDecoration.Underline else TextDecoration.None
     )
 }
 
