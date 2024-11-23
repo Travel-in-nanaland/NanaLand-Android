5d3845306644d4aa79c941f4fff660bd5ae480b7
WorldOneTop <dlwpdlf147@naver.com>
2024-11-23 14:33:01 +0900

    [release] 테스트1.1.0alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.jeju.nanaland"
         minSdk = 26
         targetSdk = 34
-        versionCode = 39
-        versionName = "1.1.0"
+        versionCode = 40
+        versionName = "1.1.1-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
