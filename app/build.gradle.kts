df4b612e2c7a0ca7b589853f5f70e68217dc405d
WorldOneTop <dlwpdlf147@naver.com>
2025-01-03 00:26:44 +0900

    [Release] 1.1.1-alpha3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.jeju.nanaland"
         minSdk = 26
         targetSdk = 34
-        versionCode = 41
-        versionName = "1.1.1-alpha2"
+        versionCode = 42
+        versionName = "1.1.1-alpha3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
