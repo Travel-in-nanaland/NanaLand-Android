13394079ce232c62a96936247c4dd4deb94e143a
WorldOneTop <dlwpdlf147@naver.com>
2025-02-01 13:21:24 +0900

    force withdraw

--- app/src/main/java/com/jeju/nanaland/ui/component/withdrawal/WithdrawalScreenLogo.kt
+++ app/src/main/java/com/jeju/nanaland/ui/component/withdrawal/WithdrawalScreenLogo.kt
@@ -2,13 +2,15 @@ package com.jeju.nanaland.ui.component.withdrawal
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.jeju.nanaland.R
 import com.jeju.nanaland.ui.theme.getColor
 import com.jeju.nanaland.ui.theme.largeTitle02
+import com.jeju.nanaland.util.resource.getString
 
 @Composable
 fun WithdrawalScreenLogo() {
     Text(
-        text = "nanaland in Jeju",
+        text = getString(R.string.app_name),
         color = getColor().main,
         style = largeTitle02
     )
